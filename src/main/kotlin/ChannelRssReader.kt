49233d7222d50ba726dcb57de5c48cbef1642d45
solchan427 <qkrthfcks@gmail.com>
2024-09-24 13:47:02 +0900

    feat: RssReader 객체 정의

---
+++ src/main/kotlin/ChannelRssReader.kt
@@ -0,0 +1,39 @@
+import org.w3c.dom.Document
+import org.w3c.dom.Node
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
+class ChannelRssReader : RssReader {
+    override fun parseDocumentToRss(document: Document): List<Rss> {
+        val rssList = mutableListOf<Rss>()
+        val nodeList = document.getElementsByTagName("item")
+
+        for (i in 0 until nodeList.length) {
+            val node = nodeList.item(i)
+            if (node.nodeType == Node.ELEMENT_NODE && node.nodeName == "item") {
+                var title = ""
+                var pubDate = ""
+                var content = ""
+
+                val childNodes = node.childNodes
+                for (j in 0 until childNodes.length) {
+                    val child = childNodes.item(j)
+                    when (child.nodeName) {
+                        "title" -> title = child.textContent.trim()
+                        "pubDate" -> pubDate = child.textContent.trim()
+                        "content:encoded" -> content = child.textContent.trim()
+                    }
+                }
+
+                val formatter = DateTimeFormatter.RFC_1123_DATE_TIME
+                val zonedDateTime = ZonedDateTime.parse(pubDate, formatter)
+
+                val localDateTime = zonedDateTime.toLocalDateTime()
+
+                rssList.add(Rss(title, localDateTime, content))
+            }
+        }
+        return rssList
+    }
+
+}
---
+++ src/main/kotlin/RssReader.kt
@@ -0,0 +1,22 @@
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.w3c.dom.Document
+import javax.xml.parsers.DocumentBuilderFactory
+
+interface RssReader {
+    suspend fun read(url: String): List<Rss> {
+        val factory = DocumentBuilderFactory.newInstance()
+        //  val document = factory.newDocumentBuilder()
+        //        .parse(url)
+
+        val document = withContext(Dispatchers.IO) {
+            factory.newDocumentBuilder()
+                .parse(url)
+        }
+
+        return parseDocumentToRss(document)
+
+    }
+
+    fun parseDocumentToRss(document: Document): List<Rss>
+}
